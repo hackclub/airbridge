3b18eea2118931f7e5ac4b38efe608000f4a92ee
Max Wofford <max@maxwofford.com>
2019-10-18 13:16:55 -0400

    Add ping endpoint

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'hello!' })
 })
 
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong!' })
+})
+
 const server = app.listen(process.env.PORT || 2345, () =>
   console.log(`Up and listening on ${server.address().port}`)
 )
