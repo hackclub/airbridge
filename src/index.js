3c714a1a0912abc149516639fd84a9e02a141338
Max Wofford <max@maxwofford.com>
2019-10-18 14:09:46 -0400

    Add caching

--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
+const env = process.env.NODE_ENV || 'development'
+if (env === 'development') {
+  require('dotenv').config()
+}
+
 import express from 'express'
+import Airtable from 'airtable'
+import redis from 'redis'
+import redisCache from 'express-redis-cache'
+const redisClient = redis.createClient(process.env.REDIS_URL)
+const cache = redisCache({ client: redisClient })
+
+const filterClubData = input => {
+  const allowed = ['Name', 'Slack Channel ID', 'Leader Slack IDs']
+  const result = {}
+  allowed.forEach(key => {
+    result[key] = input.fields[key]
+  })
+  return result
+}
+
+const operationsBase = new Airtable({
+  apiKey: process.env.AIRTABLE_API_KEY,
+}).base(process.env.AIRTABLE_OPERATIONS_TABLE)
 
 const app = express()
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello!' })
-})
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' })
 })
 
-const server = app.listen(process.env.PORT || 2345, () =>
+app.get(
+  '/',
+  cache.route({ expire: { 200: 5, 403: 60, xxx: 0 } }),
+  (req, res, next) => {
+    operationsBase('Clubs')
+      .select()
+      .all((err, records) => {
+        if (err) {
+          console.error(err)
+          res
+            .status(500)
+            .send(
+              `Received error code '${err.statusCode}' from Airtable: '${err.message}'`
+            )
+        } else {
+          const result = records.map(filterClubData)
+
+          res.status(200).json(result)
+        }
+      })
+  }
+)
+
+const server = app.listen(process.env.PORT || 2346, () =>
   console.log(`Up and listening on ${server.address().port}`)
 )
