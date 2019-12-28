121cfaa0a2437676e53148db0270d739b060f95f
Max Wofford <max@maxwofford.com>
2019-12-27 23:40:51 -0500

    pass through airtable tokens

--- src/utils.js
+++ src/utils.js
@@ -8,11 +8,17 @@ export function lookupBaseID(baseID) {
   return lookedUpID || baseID
 }
 
-export async function airtableLookup(reqParams) {
-  let { base, tableName, recordID } = reqParams
+export async function airtableLookup(params, auth) {
+  let { base, tableName } = params
   let baseID = lookupBaseID(base)
 
-  const air = new AirtablePlus({ baseID, tableName, complex: true })
-  const rawResults = await air.read()
-  return await whitelist(base, rawResults)
+  if (auth) {
+    const air = new AirtablePlus({ apiKey: auth, baseID, tableName })
+    const results = await air.read()
+    return results
+  } else {
+    const air = new AirtablePlus({ baseID, tableName, complex: true })
+    const rawResults = await air.read()
+    return await whitelist(base, rawResults)
+  }
 }
