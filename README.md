901173bd3ad04bb9c44895a5ba9f6c5056e8fcc4
Max Wofford <max@maxwofford.com>
2019-12-28 20:37:24 -0500

    Add support for choosing record fields

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 ![](api2.jpg)
 
 **Examples**
+
+**Basic calls**
 ```sh
+# Ensure you can connect
 curl https://api2.hackclub.com/ping
 # returns "pong!"
 
+# Get all records in a table
 curl https://api2.hackclub.com/v0/Operations/Badges
 # returns JSON array of records from 'Badges' table in 'Operations' base
 
+# Get specific record from a table
 curl https://api2.hackclub.com/v0/Operations/Badges/rec9j291j3k928
 # returns JSON object from airtable
+```
 
+**Authorization**
+```sh
+# Getting a protected route w/o using an auth token
 curl https://api2.hackclub.com/v0/Operations/Addresses # or some other sensative data
 # "ERROR: Tried to access table that is not in the whitelist."
 
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
-import AirtablePlus from 'airtable-plus'
-import whitelist from './whitelist'
+import Airtable from 'airtable'
+import { whitelistBaseTable, whitelistRecords } from './whitelist'
 
 export function lookupBaseID(baseID) {
   const lookedUpID = {
@@ -9,25 +9,19 @@ export function lookupBaseID(baseID) {
 }
 
 export async function airtableLookup(params, auth) {
-  let { base, tableName, view, filterByFormula, maxRecords, pageSize, sort, view, cellFormat, timeZone, userLocale, fields } = params
+  let { base, tableName, select = {} } = params
   let baseID = lookupBaseID(base)
 
   if (auth) {
-    const air = new AirtablePlus({ apiKey: auth, baseID, tableName })
-    const results = await air.read({
-      filterByFormula,
-      maxRecords,
-      pageSize,
-      sort,
-      view,
-      cellFormat,
-      timeZone,
-      userLocale,
-    })
+    const airinst = new Airtable({apiKey: auth}).base(baseID)(tableName)
+    const results = await airinst.select(select)
     return results
   } else {
-    const air = new AirtablePlus({ baseID, tableName, complex: true })
-    const rawResults = await air.read()
-    return await whitelist(base, rawResults)
+    const whitelistedFields = whitelistBaseTable(baseID, tableName, auth)
+    const airinst = new Airtable({apiKey: process.env.AIRTABLE_API_KEY}).base(baseID)(tableName)
+
+    const rawResults = await airinst.select({...select, fields: whitelistedFields}).all()
+
+    return whitelistRecords(rawResults, whitelistedFields)
   }
 }
