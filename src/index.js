8688edd4f428de3f017dc9447c79c34b7719659d
Max Wofford <max@maxwofford.com>
2020-01-07 08:58:12 -0500

    Remove outdated redis import

--- src/index.js
+++ src/index.js
@@ -9,22 +9,6 @@ if (!process.env.AIRTABLE_API_KEY) {
 import { airtableLookup } from './utils'
 import { bugsnagErrorHandler, bugsnagRequestHandler } from './bugsnag'
 import express from 'express'
-import redis from 'redis'
-import apicache from 'apicache'
-
-let cacheWithRedis
-if (process.env.REDIS_URL) {
-  const redisClient = redis.createClient(process.env.REDIS_URL)
-  cacheWithRedis = apicache.options({
-    redisClient,
-    statusCodes: { include: [200, 304] },
-    headers: {
-      'cache-control': 'no-cache',
-    },
-  }).middleware
-} else {
-  console.log('No REDIS_URL env variable found, skipping caching!')
-}
 
 const app = express()
 app.use(bugsnagRequestHandler)
@@ -37,12 +21,67 @@ app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong!' })
 })
 
-app.get('/v0/:base/:tableName?/:recordID?', async(req, res, next) => {
+app.get('/v0/:base/:tableName/:recordID', async(req, res, next) => {
   /*
-    version: api version to use. Before version 1.0 this isn't being checked– go ahead and put a 0 there
-    base: Either base ID ("apptEEFG5HTfGQE7h") or base name ("Operations")
-    tableName: Required if no recordID is provided. ex "Clubs"
-    recordID: Required if no tableName is provided. ex "rec02xw3TpmHOj7CA"
+    version: Required. api version to use. Before version 1.0 this isn't being checked– go ahead and put a 0 there
+    base: Required. Either base ID ("apptEEFG5HTfGQE7h") or base name ("Operations")
+    tableName: Required. WARNING: this table doesn't get read
+    RecordID: Required. ex "Clubs"
+  */
+  const startTime = Date.now()
+  const meta = {
+    params: {...req.params, version: 0},
+    query: {...req.query},
+  }
+  if (req.query.authKey) {
+    meta.query.authKey = '[redacted]'
+  }
+  try {
+    let providedAuth
+    if (req.headers.authorization) {
+      // example auth header "Bearer key9uu912ij9e"
+      providedAuth = req.headers.authorization.replace('Bearer ', '')
+    }
+    if (env === 'development' || env === 'test') {
+      providedAuth = req.query.authKey
+    }
+    const options = {
+      base: req.params.base,
+      tableName: req.params.tableName,
+      recordID: req.params.recordID,
+      authKey: providedAuth,
+    }
+    if (req.query.select) {
+      options.select = JSON.parse(req.query.select)
+    }
+    const result = await airtableFind(options, providedAuth)
+
+    meta.duration = Date.now() - startTime
+
+    if (req.query.meta) {
+      res.json({result, meta})
+    } else {
+      res.json(result)
+    }
+  } catch (err) {
+    console.log(err.message)
+
+    const statusCode = err.statusCode || 500
+    meta.duration = Date.now() - startTime
+    res.status(statusCode).send({
+      error: {
+        message: err.message,
+        statusCode
+      },
+      meta
+    })
+  }
+})
+app.get('/v0/:base/:tableName', async(req, res, next) => {
+  /*
+    version: Required. api version to use. Before version 1.0 this isn't being checked– go ahead and put a 0 there
+    base: Required. Either base ID ("apptEEFG5HTfGQE7h") or base name ("Operations")
+    tableName: Required. ex "Clubs"
   */
   const startTime = Date.now()
   const meta = {
