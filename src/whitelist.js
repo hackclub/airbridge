fc591c0d085f233e9644f79afc3ccea8cf897c9b
Lachlan Campbell <lachlan@hackclub.com>
2020-03-19 22:11:13 -0400

    Fix syntax error

--- src/whitelist.js
+++ src/whitelist.js
@@ -59,7 +59,7 @@ const whitelistInfo = {
     Schedule: [
       'Session Name',
       'Leader Name',
-      'Time (Eastern)'
+      'Time (Eastern)',
       'Date (formatted)',
       'Time (formatted)',
       'Calendar Event',
