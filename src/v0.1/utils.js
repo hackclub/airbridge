d69fff3fffcc736c63852c26319ba119babc0c73
Max Wofford <max@maxwofford.com>
2020-05-14 00:15:13 -0400

    Initial creation of versioning system, add v0.1

---
+++ src/v0.1/utils.js
@@ -0,0 +1,147 @@
+import Airtable from "airtable"
+import friendlyWords from "friendly-words"
+import fetch from "isomorphic-unfetch"
+import yaml from "js-yaml"
+import path from "path"
+import fs from "fs"
+
+const whitelist = (() => {
+  try {
+    const doc = yaml.safeLoad(
+      fs.readFileSync(path.resolve(__dirname, "./airtable-info.yml"), "utf8")
+    )
+    return doc
+  } catch (e) {
+    console.error(e)
+  }
+})()
+
+function lookupBaseID(name) {
+  return whitelist[name]?.baseID || base
+}
+
+function lookupBase(id) {
+  return Object.values(whitelist).find(e => e.baseID == id) || {}
+}
+
+function whitelistBaseTable(baseID, tableName) {
+  const baseInWhitelist = lookupBase(baseID)
+  if (!baseInWhitelist) {
+    const err = new Error(
+      "Not found: base either doesn't exist or isn't publicly accessible"
+    )
+    err.statusCode = 404
+    throw err
+  } else {
+    console.log("Publicly accessing base", baseID)
+  }
+
+  const tableInWhitelist = baseInWhitelist[tableName]
+  if (!tableInWhitelist) {
+    const err = new Error(
+      "Not found: table either doesn't exist or isn't publicly accessible"
+    )
+    err.statusCode = 404
+    throw err
+  } else {
+    console.log("Publicly accessing table", tableName)
+  }
+  return tableInWhitelist
+}
+
+function whitelistedRecords(records, whitelistedFields) {
+  if (Array.isArray(records)) {
+    return records.map(r => whitelistedRecords(r, whitelistedFields))
+  } else {
+    const record = records
+    const result = {
+      id: record.id,
+      fields: {},
+    }
+
+    whitelistedFields.forEach(
+      (field) => (result.fields[field] = record.fields[field])
+    )
+    return result
+  }
+}
+
+export async function airtableLookup(options, auth) {
+  const { base, tableName, select } = options
+  const baseID = lookupBaseID(base)
+
+  if (auth) {
+    const airinst = new Airtable({ apiKey: auth }).base(baseID)(tableName)
+    const rawResults = await airinst.select(select).all()
+    return rawResults.map((result) => ({
+      id: result.id,
+      fields: result.fields,
+    }))
+  } else {
+    const whitelistedFields = whitelistBaseTable(baseID, tableName, auth)
+
+    let resultFields = []
+    if (select && Array.isArray(select.fields)) {
+      resultFields = whitelistedFields.filter((f) => select.fields.includes(f))
+    } else {
+      resultFields = whitelistedFields
+    }
+
+    const airinst = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+      baseID
+    )(tableName)
+
+    const rawResults = await airinst
+      .select({ ...select, fields: resultFields })
+      .all()
+
+    return whitelistedRecords(rawResults, resultFields)
+  }
+}
+
+export async function airtableCreate(options, auth) {
+  const { base, tableName, fields } = options
+  const baseID = lookupBaseID(base)
+
+  return new Promise((resolve, reject) => {
+    const airinst = new Airtable({ apiKey: auth }).base(baseID)(tableName)
+    airinst.create(fields, (err, records) => {
+      if (err) {
+        console.error(err)
+        reject(err)
+      }
+      resolve(records)
+    })
+  })
+}
+
+function randomName() {
+  const { predicates, objects } = friendlyWords
+  const predicate = predicates[predicates.length * Math.random() - 1]
+  const object = objects[objects.length * Math.random() - 1]
+
+  return `${predicate}-${object}`
+}
+
+export async function fileToTempURL(blob, name = randomName()) {
+  const formData = new FormData()
+  formData.append("input_file", blob, name)
+  formData.append("max_views", 0)
+  formData.append("max_minutes", 1)
+  formData.append("upl", "Upload")
+
+  const response = await fetch(
+    "https://cors-anywhere.herokuapp.com/https://tmpfiles.org/?upload",
+    {
+      method: "POST",
+      mode: "cors",
+      body: formData,
+    }
+  )
+
+  if (response.headers) {
+    return res.headers.get("X-Final-Url").replace("download", "dl")
+  } else {
+    throw "Unable to create file"
+  }
+}
---
+++ src/v0/README.md
@@ -0,0 +1,71 @@
+![](api2.jpg)
+
+**Basic calls**
+```sh
+# Ensure you can connect
+curl https://api2.hackclub.com/ping
+# returns "pong!"
+
+# Get all records in a table
+curl https://api2.hackclub.com/v0/Operations/Badges
+# returns JSON array of records from 'Badges' table in 'Operations' base
+```
+
+**Authorization**
+```sh
+# Getting a protected route w/o using an auth token
+curl https://api2.hackclub.com/v0/Operations/Addresses # or some other sensative data
+# "ERROR: Tried to access table that is not in the whitelist."
+
+# Lend your own Airtable API keys to get past the whitelist
+curl -H "Authorization: Bearer VALID_AIRTABLE_KEY" https://api2.hackclub.com/v0/Operations/Addresses
+# returns JSON array of non-whitelisted records from airtable
+```
+
+**Filtering, searching**
+```sh
+# The "select" URL param can be used to pass arguments into Airtable's API. A list of optional parameters are show below with examples
+
+# Get only 1 record
+curl https://api2.hackclub.com/v0/Operations/Badges?select={"maxResults":1}
+
+# Get all badges, sorted by the number of people who have earned them
+curl https://api2.hackclub.com/v0/Operations/Badges?select={"sort":[{"field":"People Count","direction":"desc"}]}
+
+# You can combine arguments too. Here we'll sort badges by the number of people
+# and select the single record at the top of the list to get the badge with the
+# most users
+curl https://api2.hackclub.com/v0/Operations/Badges?select={"sort":[{"field":"People Count","direction":"desc"}],"maxResults":1}
+
+# You can select specific fields to return to speed up queries/only see the data you want:
+curl https://api2.hackclub.com/v0/Operations/Badges?select={"fields":["Name"]}
+```
+
+## Developing & Contributing
+
+```sh
+git clone https://github.com/hackclub/api2
+cd api2/
+pnpm install
+
+# Run locally with nodemon
+pnpm run dev
+# Run tests
+pnpm test
+```
+
+### Development tools
+
+```sh
+# The `meta` url param will return a JSON object with metadata about the request you've just made
+curl http://localhost:5000/v0/Operations/Clubs/?meta=true
+# returns the following:
+{
+    result: [...], # Array of badges from Airtable
+    meta: {
+        duration: 241, # Time in ms to complete request
+        query: {...}, # List of URL query params parsed by the server
+        params: {...}, # List of URL params parsed by the server
+    }
+}
+```
