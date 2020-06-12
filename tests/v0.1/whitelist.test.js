11390d9c6aa7bb0a18026efd0f2fe76d26a3983b
Max Wofford <max@maxwofford.com>
2020-06-12 18:16:06 -0400

    Add testing to ensure yaml bases + fields are valid

--- tests/v0.1/whitelist.test.js
+++ tests/v0.1/whitelist.test.js
@@ -1,6 +1,8 @@
 const yaml = require("js-yaml")
 const fs = require("fs")
 const path = require("path")
+const request = require("supertest")
+const app = require("../../src/index").server
 
 const whitelistPath = "../../src/v0.1/airtable-info.yml"
 
@@ -19,5 +21,35 @@ describe("load whitelist info", () => {
     expect(data["YOUR_AIRTABLE_NAME"]["baseID"]).toMatch(
       "YOUR_AIRTABLE_BASE_ID"
     )
+  })
+})
+
+describe("GET whitelisted routes", () => {
+  const routes = []
+  const tables = yaml.safeLoad(
+    fs.readFileSync(path.resolve(__dirname, whitelistPath), "utf8")
+  )
+  Object.keys(tables).forEach(tableN => {
+    const bases = tables[tableN]
+    Object.keys(bases).forEach(baseN => {
+      if (
+        tableN != "YOUR_AIRTABLE_NAME" &&
+        baseN != "baseID"
+      ) {
+        routes.push({ base: baseN, table: tableN })
+      }
+    })
+  })
+
+  routes.forEach(route => {
+    const endpointBase = `/v0.1/${route.table}/${route.base}`
+    const options = { maxRecords: 1 }
+    const endpoint = `${endpointBase}?meta=true&select=${JSON.stringify(
+      options
+    )}`
+    it(`loads ${endpointBase} with successful status code`, async () => {
+      const res = await request(app).get(endpoint)
+      expect(res.statusCode).toEqual(200)
+    })
   })
 })
