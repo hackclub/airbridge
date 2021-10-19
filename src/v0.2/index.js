271e5d8d70372924148607834b2f16ec688486de
Max Wofford <max@maxwofford.com>
2021-10-19 14:19:33 -0400

    Run fmt

--- src/v0.2/index.js
+++ src/v0.2/index.js
@@ -27,7 +27,9 @@ router.use(async (req, res, next) => {
 })
 
 router.get("/:base/:tableName", async (req, res, next) => {
-  return res.status(501).send('v0.2 GET not implemented yet! Did you mean to use v0.1?')
+  return res
+    .status(501)
+    .send("v0.2 GET not implemented yet! Did you mean to use v0.1?")
 
   if (!Object.keys(res.locals.permissions).includes(req.params.base)) {
     const error = new Error("Base not found or permissions insufficient")
@@ -80,22 +82,35 @@ router.get("/:base/:tableName", async (req, res, next) => {
 })
 
 router.post("/:base/:tableName", async (req, res, next) => {
-  const basePermission = Object.keys(res.locals.permissions).includes(req.params.base)
+  const basePermission = Object.keys(res.locals.permissions).includes(
+    req.params.base
+  )
   if (!basePermission) {
     const error = new Error("Base not found or permissions insufficient")
     error.statusCode = 404
     return next(error)
   }
-  const tablePermission = Object.keys(res.locals.permissions[req.params.base]).includes(req.params.tableName)
+  const tablePermission = Object.keys(
+    res.locals.permissions[req.params.base]
+  ).includes(req.params.tableName)
   if (!tablePermission) {
     const error = new Error("Table not found or permissions insufficient")
     error.statusCode = 404
     return next(error)
   }
-  const unpermittedFields = Object.keys(req.body).filter(field => !res.locals.permissions[req.params.base][req.params.tableName].post.includes(field))
+  const unpermittedFields = Object.keys(req.body).filter(
+    (field) =>
+      !res.locals.permissions[req.params.base][
+        req.params.tableName
+      ].post.includes(field)
+  )
   const fieldPermission = unpermittedFields.length === 0
   if (!fieldPermission) {
-    const error = new Error(`Field(s) ${unpermittedFields.map(f => `'${f}'`).join(', ')} doesn't exist or permissions insufficient`)
+    const error = new Error(
+      `Field(s) ${unpermittedFields
+        .map((f) => `'${f}'`)
+        .join(", ")} doesn't exist or permissions insufficient`
+    )
     error.statusCode = 422
     return next(error)
   }
@@ -106,20 +121,26 @@ router.post("/:base/:tableName", async (req, res, next) => {
   const airinst = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
     ab
   )(at)
-  const createValues = Array.isArray(req.body) ? {fields: req.body} : [{fields: req.body}]
+  const createValues = Array.isArray(req.body)
+    ? { fields: req.body }
+    : [{ fields: req.body }]
   const rawResults = await airinst
     .create(createValues)
     .catch((err) => console.log(err))
-  
-  const rawResultArray = Array.isArray(rawResults) ? rawResults : [rawResults]
-  const results = rawResults.map(rec => {
-    const result = { id: rec.id, fields: {} }
-    const readPermissions = res.locals.permissions[req.params.base][req.params.tableName]?.get || []
-    const writePermissions = res.locals.permissions[req.params.base][req.params.tableName].post
 
-    const allowedFields = Array.from([...new Set([...readPermissions, ...writePermissions])])
-    console.log({allowedFields})
-    allowedFields.forEach(field => {
+  const rawResultArray = Array.isArray(rawResults) ? rawResults : [rawResults]
+  const results = rawResults.map((rec) => {
+    const result = { id: rec.id, fields: {} }
+    const readPermissions =
+      res.locals.permissions[req.params.base][req.params.tableName]?.get || []
+    const writePermissions =
+      res.locals.permissions[req.params.base][req.params.tableName].post
+
+    const allowedFields = Array.from([
+      ...new Set([...readPermissions, ...writePermissions]),
+    ])
+    console.log({ allowedFields })
+    allowedFields.forEach((field) => {
       result.fields[field] = rec.fields[field]
     })
     return result
@@ -133,7 +154,7 @@ router.post("/:base/:tableName", async (req, res, next) => {
 })
 
 router.patch("/:base/:tableName", async (req, res, next) => {
-  res.status(501).send('v0.2 PATCH not implemented yet!')
+  res.status(501).send("v0.2 PATCH not implemented yet!")
 })
 
 router.get("/test", async (req, res, next) => {
