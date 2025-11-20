b3083930ff7e86a3b15d1bc67154a6dfc0495368
Max Wofford <max@maxwofford.com>
2025-11-20 11:51:20 -0500

    Add filter for protected words in formulas

--- src/v0.1/utils.js
+++ src/v0.1/utils.js
@@ -4,6 +4,26 @@ import yaml from "js-yaml"
 import path from "path"
 import fs from "fs"
 
+export function ensureFormulaSafe(formula) {
+  const protectedWords = "SUBSTITUTE LEFT RIGHT MID FIND SEARCH LEN".split(" ")
+  // fail if any protected words are found in the formula
+  let usedWords = []
+
+  protectedWords.forEach((word) => {
+    if (formula.toLowerCase().includes(word.toLowerCase())) {
+      usedWords.push(word)
+    }
+  })
+
+  if (usedWords.length > 0) {
+    const err = new Error(
+      `Formula contains protected words: ${usedWords.join(", ")}`
+    )
+    err.statusCode = 400
+    throw err
+  }
+}
+
 const allowlist = (() => {
   try {
     const doc = yaml.load(
@@ -75,6 +95,11 @@ function allowlistedRecords(records, allowlistedFields) {
 
 export async function airtableLookup(options, auth) {
   const { base, tableName, select } = options
+
+  if (select?.filterByFormula) {
+    ensureFormulaSafe(select.filterByFormula)
+  }
+
   const baseID = lookupBaseID(base)
 
   if (auth) {
